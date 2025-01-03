cdc256b318ff20bc79e6e072d0709f2027973c79
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-03 10:44:08 +0300

    made files

---
+++ question1.js
@@ -0,0 +1,6 @@
+//What is JavaScript, and how does it differ from HTML and CSS?
+
+//CSS defines the visual style and layout of the website's elements.
+//HTML allows us to create content like text, buttons, and other components.
+//JavaScript is a programming language that allows us to add interactivity to our website, like animations,
+//HTML and CSS are used to design and structure a website
