26e95a12bc81a85fffcbd92802870a32aba36db5
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-03 15:29:19 +0300

    done

--- question1.js
+++ question1.js
@@ -3,4 +3,4 @@
 //CSS defines the visual style and layout of the website's elements.
 //HTML allows us to create content like text, buttons, and other components.
 //JavaScript is a programming language that allows us to add interactivity to our website, like animations,
-//HTML and CSS are used to design and structure a website
+//HTML and CSS are used to design and structure a website.
