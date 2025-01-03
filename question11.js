97f296efd3ac6ca36ce135532e2e4ff5cdbf795a
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-03 15:51:45 +0300

    done

---
+++ question11.js
@@ -0,0 +1,12 @@
+// What is the difference between == and === in JavaScript?
+
+// == checks if the values are equal, while === checks if the values are equal and the types are also equal.
+
+console.log(1 == "1"); // true
+// In the first example, the values are equal, but the types are different (number vs string)
+
+
+console.log(1 === "1"); // false
+// In the second example, the values are not equal because the types are different.
+
+
