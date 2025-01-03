fb5e62ee2d397cb08de471beb9877540023c434f
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-03 15:37:40 +0300

    done

---
+++ question2.js
@@ -0,0 +1,10 @@
+//How can you include JavaScript in an HTML file? (Write an example.)
+
+
+// Internal: In this, you add <script> within the head or the body section in the html file.
+// External: In this, you use the <script> alongside with the src in the head section
+// Inline: In this, you use the "onclick" attribute directly within the html elements.
+
+<script>
+    console.log("This is how to include JS in a HTML file")
+</script>
