ddae6431b7a615ada5ad54ed8096add66ad2d1fd
M-Saleh-N <salehnasser65784@gmail.com>
2025-01-03 15:41:50 +0300

    done

---
+++ question4.js
@@ -0,0 +1,8 @@
+//What are the differences between var, let, and const?
+
+
+//var is function scoped, let and const are block scoped
+//let and const are more secure and prevent bugs
+//const is used for variables that should not be changed
+//let is used for variables that can be changed
+//var is used for variables that can be changed
